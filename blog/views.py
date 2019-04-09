cf49d6415bbd47b83313ee7bddf76fdceb301311
Gary Waller <garywaller246@gmail.com>
2019-04-09 16:56:45 +0300

    templates and css

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+
+posts = [
+    {
+        'author': 'Gary Waller',
+        'title': 'Initial post',
+        'content': 'First!!!',
+        'date_posted': 'January 1, 2019'
+    },
+    {
+        'author': 'Yael',
+        'title': 'Second post',
+        'content': 'Hello Dad',
+        'date_posted': 'March 30, 2019'
+    },
+
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'} )
+  
