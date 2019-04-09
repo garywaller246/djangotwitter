6e1de772ee214259caf9c3de386c0e141eb5e316
Gary Waller <garywaller246@gmail.com>
2019-04-09 18:13:27 +0300

    db and migrations setup

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Gary Waller',
-        'title': 'Initial post',
-        'content': 'First!!!',
-        'date_posted': 'January 1, 2019'
-    },
-    {
-        'author': 'Yael',
-        'title': 'Second post',
-        'content': 'Hello Dad',
-        'date_posted': 'March 30, 2019'
-    },
-
-]
+from .models import Post  
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
